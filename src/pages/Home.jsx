b99ff83c7eeee396e9491c12b33155c26822c1a6
Layljan <ljdumayas@gmail.com>
2024-06-17 18:18:29 +0800

    Added taillwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Home() {
   return (
-    <div className="me">
+    <div className="PS m-10">
       <p className="name">LYLE JOHN DUMAYAS</p>
       <p>Ako si lyle and ito ang small description kung sino ako</p>
     </div>
