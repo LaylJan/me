3eaed25c3a0ba705f0bd5207874d20c18d6c95ab
Layljan <ljdumayas@gmail.com>
2024-06-22 17:25:58 +0800

    Added Neu Picture

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
-import Tagumpay from "../assets/Tagumpay.png";
+import TNHS from "../assets/Tagumpay.png";
+import NEU from "../assets/New Era.svg";
 
 const Education = ({ setPage }) => {
   return (
     <div className="PS text-justify">
       <p className="text-lg">Educational Background:</p>
-      <div className="flex mt-5 mb-5 ml-5 items-start space-x-5">
-        <img src={Tagumpay} alt="Tagumpay" className="w-28" />
+      <div className="flex mt-5 mb-5 ml-5 items-center space-x-5">
+        <img src={TNHS} alt="Tagumpay" className="w-32" />
         <div className="flex flex-col">
-          <p className="text-base">Tagumpay National High School</p>
-          <p className="text-base">2015 - 2019</p>
-          <p className="text-base">Rodriguez, Rizal</p>
-          <p className="text-base">Junior High School</p>
+          <p className="text-sm">Tagumpay National High School</p>
+          <p className="text-sm">2015 - 2019</p>
+          <p className="text-sm">Rodriguez, Rizal</p>
+          <p className="text-sm">Junior High School</p>
+        </div>
+      </div>
+      <div className="flex mt-5 mb-5 ml-5 items-center space-x-5">
+        <img src={NEU} alt="New Era" className="w-32" />
+        <div className="flex flex-col">
+          <p className="text-sm">New Era University</p>
+          <p className="text-sm">2019 - 2021</p>
+          <p className="text-sm">Quezon City</p>
+          <p className="text-sm">
+            TVL - Information and Communication Technology{" "}
+          </p>
         </div>
       </div>
     </div>
