755260a3f39b4bbba39b1ed7db6912ce28d0a157
Layljan <ljdumayas@gmail.com>
2024-06-22 21:40:05 +0800

    Fix: Fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 
 const Home = ({ setPage }) => {
   return (
-    <div className="m-5 ">
-      <p className="text-white text-5xl mb-5 font-LS font-black">
+    <div className="m-5 font-ls ">
+      <p className="text-white text-5xl mb-5 font-black">
         I'M LYLE JOHN DUMAYAS
       </p>
-      <p className="text-justify text-base">
+      <p className="text-white text-justify font-semibold text-lg">
         Im a web developer with 1 year of experience worked on a few projects
         and ready to bring your designs to reality.
       </p>
-      <div className="flex flex-col items-start m-8">
-        <button className="hover:text-white">Education</button>
+      <div className="flex flex-col items-start m-8 text-lg font-semibold text-white">
+        <button className="hover:underline">Education</button>
         <button
           onClick={() => setPage("EXPERIENCE")}
-          className="hover:text-white"
+          className="hover:underline"
         >
           Experiences
         </button>
-        <button className="hover:text-white">Skills</button>
-        <button className="hover:text-white">Projects</button>
+        <button className="hover:underline">Skills</button>
+        <button className="hover:underline">Projects</button>
       </div>
     </div>
   );
