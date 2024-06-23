a8cb8e75eaa78f0152c055bffe3bc0eb749ec7ea
Layljan <ljdumayas@gmail.com>
2024-06-23 19:23:04 +0800

    Added more components

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import Java from "../assets/Java.png";
+import RJS from "../assets/Reactjs.png";
 
 const Skills = ({ setPage }) => {
   return (
     <div className="font-LS text-justify">
       <p className="text-3xl font-black">SKILLS:</p>
+      <div className="flex text-xl font-medium mt-2 mb-5 ml-5 items-center space-x-5">
+        <img src={RJS} alt="IntelliSeven" className="w-32" />
+        <div className="flex flex-col">
+          <p className="font-bold">React</p>
+          <div className="flex flex-col">
+            <p className="">
+              Through my internship at IntelliSeven Inc., where we primarily
+              used React.js and Vite, I became confident in my skills with
+              React. In fact this portfolio is made with react js.
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="flex text-xl font-medium mt-2 mb-5 ml-5 items-center space-x-5">
         <img src={Java} alt="IntelliSeven" className="w-32" />
         <div className="flex flex-col">
