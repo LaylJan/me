f81dded2900ae40c72774dd5a54f71fa903295dd
Layljan <ljdumayas@gmail.com>
2024-07-10 16:41:57 +0800

    New Design for Experience tab

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -7,16 +7,24 @@ const Experiences = ({ goto }) => {
       <p className="text-2xl md:text-3xl font-black">EXPERIENCES:</p>
       <div
         onClick={() => goto("https://www.intelliseven.com.ph/")}
-        className="flex text-base md:text-xl font-medium mt-2 mb-5 md:ml-5 items-center space-x-5 p-2 hover:bg-white hover:bg-opacity-10 transition duration-200"
+        className="mt-2 md:ml-5 hover:bg-white hover:bg-opacity-10 transition duration-200"
       >
-        <img src={I7} alt="IntelliSeven" className="w-20 md:w-28" />
-        <div className="flex flex-col -space-y-1">
-          <p className="font-bold">IntelliSeven Solutions Inc.</p>
-          <div className="flex flex-col">
-            <p className="">February 2024 - May 2024</p>
-            <p className="">Corporate office in Quezon City</p>
-            <p className="">Internship(OJT)</p>
+        <div className="flex text-base md:text-xl font-medium mt-2 md:ml-5 items-center space-x-5">
+          <img src={I7} alt="IntelliSeven" className="w-20 md:w-28" />
+          <div className="flex flex-col -space-y-1">
+            <p className="font-bold">IntelliSeven Solutions Inc.</p>
+            <div className="flex flex-col">
+              <p className="">February 2024 - May 2024</p>
+              <p className="">Corporate office in Quezon City</p>
+              <p className="">Internship(OJT)</p>
+            </div>
           </div>
+        </div>
+        <div className="text-base md:text-xl font-medium mb-5 md:ml-5 items-center md:space-x-5 p-2">
+          <p className="">
+            Contributed to the development of a chat admin project, focusing on
+            both front-end and back-end tasks.
+          </p>
         </div>
       </div>
     </div>
