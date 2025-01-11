ca5bac35ddfa0f6477f67ab9b3294ac483474fd2
Layljan <ljdumayas@gmail.com>
2025-01-11 16:34:08 +0800

    feat: New skill css

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,6 +4,7 @@ import RJS from "../assets/Reactjs.png";
 import WD from "../assets/WD.png";
 import MS from "../assets/MS.png";
 import PR from "../assets/Programming.png";
+import CSS from "../assets/CSS.png";
 
 const Skills = ({ setPage }) => {
   return (
@@ -20,6 +21,23 @@ const Skills = ({ setPage }) => {
             <p className="">
               Equipped with knowledge and job experience in frontend tasks using{" "}
               JavaScript frameworks such as <b>React</b> and <b>Vue</b>.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex text-base md:text-xl font-medium mt-2 mb-5 md:ml-5 items-center md:space-x-5 p-2">
+        <img src={CSS} alt="Webdev" className="w-24 hidden md:block" />
+        <div className="flex flex-col -space-y-1">
+          <div className="flex items-center">
+            <img src={CSS} alt="Webdev" className="w-4 mr-1 -mt-1 md:hidden" />
+            <p className="font-bold text-2xl md:text-xl">
+              Computer System Servicing
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p className="">
+              With basic skills in assembling computers, installing software,
+              setting up networks and other common issues.
             </p>
           </div>
         </div>
