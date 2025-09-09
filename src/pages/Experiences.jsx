24252882f6f8774e5aff6b3e7b252a71fc5c1963
layljan <layljandee@gmail.com>
2025-09-09 14:56:30 +0800

    Added Work experience

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import I7 from "../assets/IntelliSeven.png";
+import BUGC from "../assets/BUGC.png";
 import Certificates from "./Certificates";
 
 const Experiences = ({ goto }) => {
@@ -11,21 +12,20 @@ const Experiences = ({ goto }) => {
         className="mt-2 md:ml-5 hover:bg-white hover:bg-opacity-10 transition duration-200"
       >
         <div className="flex text-base md:text-xl font-medium mt-2 md:ml-5 items-center space-x-5">
-          <img src={I7} alt="IntelliSeven" className="w-20 md:w-24" />
+          <img src={BUGC} alt="IntelliSeven" className="w-20 md:w-24" />
           <div className="flex flex-col -space-y-1">
-            <p className="font-bold">IntelliSeven Solutions Inc.</p>
+            <p className="font-bold">Britannica United Motors</p>
             <div className="flex flex-col">
-              <p className="">February 2024 - May 2024</p>
-              <p className="">Corporate office in Quezon City</p>
-              <p className="">Internship</p>
+              <p className="">June 2025 – Present</p>
+              <p className="">Automotive dealership group</p>
+              <p className="">IT Assistant</p>
             </div>
           </div>
         </div>
         <div className="text-base md:text-xl font-medium mb-5 md:ml-5 items-center md:space-x-5 p-2">
           <p className="">
-            Contributed to the development of a chat admin project, focusing on
-            both front-end and back-end tasks mainly using React.js with
-            Graphql.
+            Provide regional on-site and remote IT support for hardware and software issues across Luzon branches,
+            ensuring optimal functionality of all systems and peripherals to maintain uninterrupted operations.
           </p>
         </div>
       </div>
